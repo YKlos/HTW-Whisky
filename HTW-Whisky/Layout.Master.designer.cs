9adb173239d84b17d14eaf972df3fe643436e4fa
ro0f <accounts@eric-eifler.net>
2011-06-16 15:17:07 +0200

    MasterPage mit Layout und CSS, TestPage

---
+++ HTW-Whisky/Layout.Master.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Der Code wurde von einem Tool generiert.
+//
+//     Änderungen an der Datei führen möglicherweise zu falschem Verhalten, und sie gehen verloren, wenn
+//     der Code erneut generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace HTW_Whisky {
+    
+    
+    public partial class Site1 {
+        
+        /// <summary>
+        /// head-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// phHeader-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder phHeader;
+        
+        /// <summary>
+        /// phNav-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder phNav;
+        
+        /// <summary>
+        /// phMain-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder phMain;
+        
+        /// <summary>
+        /// phToolbar-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder phToolbar;
+    }
+}
---
+++ HTW-Whisky/TestPage.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout.Master" AutoEventWireup="true" CodeBehind="TestPage.aspx.cs" Inherits="HTW_Whisky.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="phHeader" runat="server">
+Dies ist eine toller Header!
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="phNav" runat="server">
+Dies ist eine tolle Navigation!
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="phMain" runat="server">
+Das ist der Inhalt
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="phToolbar" runat="server">
+Das ist die Toolbox
+</asp:Content>
