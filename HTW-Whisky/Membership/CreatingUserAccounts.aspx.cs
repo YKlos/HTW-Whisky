67d671ee1b3e0003b75d85eaa2e5067ea235c19a
t0bias <tobias.lana@googlemail.com>
2011-07-20 14:02:53 +0200

    Register User in DB

--- HTW-Whisky/Membership/CreatingUserAccounts.aspx.cs
+++ HTW-Whisky/Membership/CreatingUserAccounts.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace HTW_Whisky.Membership
 {
-    public partial class CreatingUserAccounts : System.Web.UI.Page
+    public partial class CreateUserAccount : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
