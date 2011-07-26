e008bc10a0ac1566368a989ee9dd81e4e7334a60
YKlos <yassir.klos@googlemail.com>
2011-07-26 11:53:02 +0200

    Bilderupload hintzugefuegt

--- HTW-Whisky/Whisky/AddWhisky.aspx.cs
+++ HTW-Whisky/Whisky/AddWhisky.aspx.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Data;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using HTW_Whisky.App_Code.WhiskyAppDatasetTableAdapters;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace HTW_Whisky
 {
@@ -19,8 +17,77 @@ namespace HTW_Whisky
         protected void btnAddWhisky_Click(object sender, EventArgs e)
         {
             whiskyTableAdapter WhiskyAdapter = new whiskyTableAdapter();
-            WhiskyAdapter.Insert(tbxWhiskyName.Text, tbxAroma.Text, tbxGeschmack.Text, tbxAbgang.Text, tbxFasstyp.Text, tbxBeschreibung.Text, int.Parse(tbxJahrgang.Text), double.Parse(tbxAlkoholgehalt.Text), double.Parse(tbxLiter.Text), ddlTyp.SelectedIndex, cbxAktiv.Checked);
-            lblWhiskyAdded.Text = "Whiskey hinzugefügt";
+            WhiskyAdapter.Insert(tbxWhiskyName.Text, tbxAroma.Text,
+                                 tbxGeschmack.Text, tbxAbgang.Text,
+                                 tbxFasstyp.Text, tbxBeschreibung.Text,
+                                 int.Parse(tbxJahrgang.Text), double.Parse(tbxAlkoholgehalt.Text),
+                                 double.Parse(tbxLiter.Text), ddlTyp.SelectedIndex, cbxAktiv.Checked);
+
+            int whiskyID = (int)WhiskyAdapter.GetWhiskyByParams(tbxWhiskyName.Text, tbxAroma.Text,
+                                                          tbxGeschmack.Text, tbxAbgang.Text,
+                                                          tbxFasstyp.Text, tbxBeschreibung.Text,
+                                                          int.Parse(tbxJahrgang.Text), double.Parse(tbxAlkoholgehalt.Text),
+                                                          double.Parse(tbxLiter.Text), ddlTyp.SelectedIndex, cbxAktiv.Checked);
+
+            //Bildupload
+
+            if (FileUpload1.PostedFile != null && FileUpload1.PostedFile.FileName != "")
+            {
+                byte[] imageSize = new byte[FileUpload1.PostedFile.ContentLength];
+                HttpPostedFile uploadedImage = FileUpload1.PostedFile;
+                uploadedImage.InputStream.Read(imageSize, 0, (int)FileUpload1.PostedFile.ContentLength);
+
+
+                pictureTableAdapter PictureAdapter = new pictureTableAdapter();
+                PictureAdapter.Insert(whiskyID, cbxFreigabe.Checked, cbxAktiv.Checked, imageSize);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            /*
+	        // Pfad zum speichern der Bilder festlegen
+            string savePath = Request.PhysicalApplicationPath + "images\\";
+	        // wenn ein File ausgewählt wurde
+            if (FileUpload1.HasFile)
+            {
+                string fileName = FileUpload1.FileName; // dieser Name sollte eindeutig ein !
+                savePath += fileName;
+                FileUpload1.SaveAs(savePath);
+                bool BitmapOK = true;
+                bool GrößeOK = true;
+                bool DateigrößeOK = true;
+                FileInfo FI = new FileInfo(savePath);
+		        // überprüfen ob die Datei eine Bestimmte Dateigröße nicht überschreitet, hier sind es 100 kB
+                if (FI.Length > 100 * 1024)
+                {
+                    DateigrößeOK = false;
+                    File.Delete(savePath);
+                }
+                try
+                {
+			        // Überprüfen ob die Datei gültiges Bild ist
+                    System.Drawing.Bitmap BM = new System.Drawing.Bitmap(savePath);
+			        // Breite und Höhe prüfen , hier sind es 400 mal 400 Pixel
+                    if (BM.Width > 400 || BM.Height > 400)
+                    {
+                        GrößeOK = false;
+                        BM.Dispose();
+                        File.Delete(savePath);
+                    }
+                }
+                catch
+                {
+                    BitmapOK = false;
+                    File.Delete(savePath);
+                }
+		        // wenn alles OK war dann kann das Bild hier weiterverarbeitet werden, wenn nicht, können entsprechende Fehlermeldungen ausgegben werden
+                if (BitmapOK && GrößeOK && DateigrößeOK)
+                {
+                    Label1.Text = "Hochgeladen!!!!!!!!!!1111einself"; 
+                }
+            }*/
         }
     }
 }
--- HTW-Whisky/Whisky/AddWhisky.aspx.designer.cs
+++ HTW-Whisky/Whisky/AddWhisky.aspx.designer.cs
@@ -229,6 +229,33 @@ namespace HTW_Whisky {
         protected global::System.Web.UI.WebControls.Button btnAddWhisky;
         
         /// <summary>
+        /// FileUpload1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+        
+        /// <summary>
+        /// Button1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// cbxFreigabe-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox cbxFreigabe;
+        
+        /// <summary>
         /// lblWhiskyAdded-Steuerelement
         /// </summary>
         /// <remarks>
