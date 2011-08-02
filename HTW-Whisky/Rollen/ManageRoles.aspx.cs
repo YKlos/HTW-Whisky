43236e30da4279256105b26fc931761bc971bd28
t0bias <tobias.lana@googlemail.com>
2011-08-02 21:36:55 +0200

    Ordner Roles in Rollen umbenannt

---
+++ HTW-Whisky/Rollen/ManageRoles.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace HTW_Whisky.Roles
+{
+    public partial class ManageRoles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+                DisplayRolesInGrid();
+        }
+
+        protected void CreateRoleButton_Click(object sender, EventArgs e)
+        {
+            string newRoleName = RoleName.Text.Trim();
+            if (!System.Web.Security.Roles.RoleExists(newRoleName)) 
+                // Create the role 
+                System.Web.Security.Roles.CreateRole(newRoleName);
+                RoleName.Text = string.Empty;
+        }
+
+        private void DisplayRolesInGrid()
+        {
+            RoleList.DataSource = System.Web.Security.Roles.GetAllRoles();
+            RoleList.DataBind();
+            Label1.Text = "aktualisiert";
+        }
+
+        protected void RoleList_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            Label1.Text = "RoleList_RowDeleting";
+            Label RoleNameLabel = RoleList.Rows[e.RowIndex].FindControl("RoleNameLabel") as Label;
+            // Delete the role 
+            System.Web.Security.Roles.DeleteRole(RoleNameLabel.Text);
+            // Rebind the data to the RoleList grid
+            // DisplayRolesInGrid();
+        }
+
+        protected void RoleList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label1.Text = "RoleList_SelectedIndexChanged";
+        }
+    }
+}
---
+++ HTW-Whisky/Rollen/UsersAndRoles.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Der Code wurde von einem Tool generiert.
+//
+//     Änderungen an der Datei führen möglicherweise zu falschem Verhalten, und sie gehen verloren, wenn
+//     der Code erneut generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace HTW_Whisky.Roles {
+    
+    
+    public partial class UserAndRoles {
+        
+        /// <summary>
+        /// ActionStatus-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ActionStatus;
+        
+        /// <summary>
+        /// UserList-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList UserList;
+        
+        /// <summary>
+        /// UsersRoleList-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater UsersRoleList;
+    }
+}
