201fcc4706345e9c087c5b0ce19f0af5056fb896
t0bias <tobias.lana@googlemail.com>
2011-08-15 15:41:45 +0200

    Tastingstuff

---
+++ HTW-Whisky/Whisky/Tastings.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HTW_Whisky.App_Code.WhiskyAppDatasetTableAdapters;
+
+
+namespace HTW_Whisky.Whisky
+{
+    public partial class Tastings : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                fillGridViews();
+            }
+        }
+
+        private void fillGridViews()
+        {
+            tastingTableAdapter TastingAdapter = new tastingTableAdapter();
+            Guid usersuid = (Guid)System.Web.Security.Membership.GetUser().ProviderUserKey;
+            gvOwnTastings.DataSource = TastingAdapter.GetOwnTastings(usersuid);
+            gvFriendsTastings.DataSource = TastingAdapter.GetFriendsTastings(usersuid);
+            gvOwnTastings.DataBind();
+            gvFriendsTastings.DataBind();
+        }
+    }
+}
