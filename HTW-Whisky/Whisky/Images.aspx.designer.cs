c686b93802512c806fdf2112e442f953ae991dd0
Eric Eifler <info@eric-eifler.net>
2011-08-16 01:18:01 +0200

    bugfixing

--- HTW-Whisky/Whisky/Images.aspx.designer.cs
+++ HTW-Whisky/Whisky/Images.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace HTW_Whisky.Whisky {
         protected global::System.Web.UI.WebControls.Button btnUploadImage;
         
         /// <summary>
+        /// Label1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
         /// panImages-Steuerelement
         /// </summary>
         /// <remarks>
